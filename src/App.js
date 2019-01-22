46416bb543fc197b1b02ec8ff2c46224b822bedd
Stuart Sandine <stuajc@gmail.com>
2019-01-22 10:39:29 -0800

    initial commit: basic SPA via create-react-app, auth via aws amplify / cognito

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
@@ -25,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
