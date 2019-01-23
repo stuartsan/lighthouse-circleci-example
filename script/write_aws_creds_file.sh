c4b782afd6bc7bf0b4f250e925d0eb86b4b06da9
Stuart Sandine <stuajc@gmail.com>
2019-01-22 20:15:40 -0800

    needs also ~/.aws/config

--- script/write_aws_creds_file.sh
+++ script/write_aws_creds_file.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-mkdir ~/.aws/
-echo "[amplify]\naws_access_key_id=$AWS_ACCESS_KEY_AMPLIFY\naws_secret_access_key=$AWS_SECRET_KEY_AMPLIFY" > ~/.aws/credentials
+mkdir -p ~/.aws/
+
+cat <<EOF > ~/.aws/credentials
+[amplify]
+aws_access_key_id=$AWS_ACCESS_KEY_AMPLIFY
+aws_secret_access_key=$AWS_SECRET_KEY_AMPLIFY
+EOF
+
+cat <<EOF > ~/.aws/config
+[profile amplify]
+region=us-west-2
+EOF
