57f99338e7c729025a6e4b0c228cd40cdabd9311
Stuart Sandine <stuajc@gmail.com>
2019-01-24 15:48:25 -0800

    basic bundlesize audit

---
+++ lighthouse-config/custom-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'lighthouse:default',
+
+  audits: [
+    'bundlesize-audit',
+  ],
+
+  categories: {
+    bundlesize: {
+      title: 'JS Bundle Size',
+      description: 'Can we keep it under the threshold???',
+      auditRefs: [
+        // When we add more custom audits, `weight` controls how they're averaged together.
+        {id: 'bundlesize-audit', weight: 1},
+      ],
+    },
+  },
+};
