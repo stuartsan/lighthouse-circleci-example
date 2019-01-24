b0e8bb918c1b2a6ddf46c6cef81589afb208fecc
Stuart Sandine <stuajc@gmail.com>
2019-01-24 10:02:47 -0800

    send a better pr comment

--- script/analyze_scores.js
+++ script/analyze_scores.js
@@ -8,18 +8,24 @@ const bot = require("circle-github-bot").create();
 const pkg = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 const requiredScores = pkg.lighthouse.requiredScores;
 
-const actualScores = [];
+const reportsDir = process.argv[3];
 
-fs.readdirSync(process.argv[3]).forEach(file => {
+const jsonReports = [];
+const htmlReportFilenames = [];
+
+
+fs.readdirSync(reportsDir).forEach(file => {
   if (path.extname(file) === '.json') {
-    actualScores.push(JSON.parse(fs.readFileSync(path.resolve(process.argv[3], file), 'utf8')));
-  }
-})
+    jsonReports.push(JSON.parse(fs.readFileSync(path.resolve(reportsDir, file), 'utf8')));
 
+  } else if (path.extname(file) === '.html') {
+    htmlReportFilenames.push(file);
+  }
+});
 
 let success = true;
 
-const scoresAcrossRunsByCategory = actualScores.reduce((acc, run) => {
+const scoresAcrossRunsByCategory = jsonReports.reduce((acc, run) => {
   Object.keys(requiredScores).forEach(category => {
     acc[category] = acc[category] || [];
     acc[category].push(run.categories[category].score * 100);
@@ -27,30 +33,53 @@ const scoresAcrossRunsByCategory = actualScores.reduce((acc, run) => {
   return acc;
 }, {});
 
-console.log('------------------------------------------');
-console.log(`Number of parallel test runs: ${actualScores.length}`);
-console.log('------------------------------------------');
+
+const ciStdout = [
+  '------------------------------------------',
+  `Number of parallel test runs: ${jsonReports.length}`,
+  '------------------------------------------',
+];
+
+const prComment = [
+  '<h2>Lighthouse scores</h2>',
+  `<p><strong>Number of parallel test runs:</strong>: ${jsonReports.length}</p>`,
+  `<p>The best scores across all runs are shown below.</p>`,
+];
 
 Object.keys(requiredScores).forEach(category => {
   const requiredOutOf100 = requiredScores[category];
   const actualBestOutOf100 = Math.max.apply(null, scoresAcrossRunsByCategory[category]);
 
+
   if (actualBestOutOf100 < requiredScores[category]) {
-    console.log(`❌ ${category}: ${actualBestOutOf100}/${requiredOutOf100}`);
+    ciStdout.push(`❌ ${category}: ${actualBestOutOf100}/${requiredOutOf100}`);
+    prComment.push(`<h3>❌ ${category}</h3>`);
+    prComment.push(`<p>${actualBestOutOf100}/${requiredOutOf100}</p>`);
     success = false;
   } else {
-    console.log(`✅ ${category}: ${actualBestOutOf100}/${requiredOutOf100}`);
+    ciStdout.push(`✅ ${category}: ${actualBestOutOf100}/${requiredOutOf100}`);
+    prComment.push(`<h3>✅ ${category}</h3>`);
+    prComment.push(`<p>${actualBestOutOf100}/${requiredOutOf100}</p>`);
   }
 
   const areAllScoresEqual = scoresAcrossRunsByCategory[category]
     .every((val, i, arr) => val === scoresAcrossRunsByCategory[category][0]);
 
   if (!areAllScoresEqual) {
-    console.log(`   - scores varied across runs: [${scoresAcrossRunsByCategory[category].join(' ')}]`)
+    ciStdout.push(`   - scores varied across runs: [${scoresAcrossRunsByCategory[category].join(' ')}]`)
+    prComment.push(`<p>Scores varied across runs: [${scoresAcrossRunsByCategory[category].join(' ')}]</p>`)
   }
 });
 
-bot.comment(process.env.GITHUB_OAUTH_TOKEN, '<p>what up this is an automated message</p>');
+prComment.push('<h3>Detailed reports</h3>');
+htmlReportFilenames.forEach((filename, idx) => {
+  prComment.push(`<p>bot.artifactLink(`reports/${filename}`, `Run ${idx + 1}`)</p>`);
+});
+
+console.log(ciStdout.join('\n'));
+console.log(prComment.join('\n'));
+
+bot.comment(process.env.GITHUB_OAUTH_TOKEN, prComment.join('\n'));
 
 if (!success) {
   process.exit(1);
